4f59b81fcd47e5dcc2cfd7efac5504e61c563181
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-04 17:31:41 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require hello_world
-//= require doesn_nothing
+//= require main
+//= require alert
+//= require wrong_place_wrong_time
